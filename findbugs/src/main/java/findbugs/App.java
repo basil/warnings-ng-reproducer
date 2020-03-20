51627493c733d27f8e44ea5335d1ea575e2b5427
Basil Crow <me@basilcrow.com>
2020-03-20 12:11:07 -0700

    Format Java files

--- findbugs/src/main/java/findbugs/App.java
+++ findbugs/src/main/java/findbugs/App.java
@@ -1,13 +1,13 @@
 package findbugs;
 
 public class App {
-    public String getGreeting() {
-        String foo = null;
-        int bar = foo.length();
-	return foo;
-    }
+  public String getGreeting() {
+    String foo = null;
+    int bar = foo.length();
+    return foo;
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
