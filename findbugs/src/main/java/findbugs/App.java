75076730b4a560ee617cc3221505537459b8474b
Basil Crow <me@basilcrow.com>
2020-03-20 12:08:06 -0700

    Initial commit.

---
+++ findbugs/src/main/java/findbugs/App.java
@@ -0,0 +1,13 @@
+package findbugs;
+
+public class App {
+    public String getGreeting() {
+        String foo = null;
+        int bar = foo.length();
+	return foo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
